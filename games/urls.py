361035c17697678f0e13d7666363a2fcba35e0e3
SAYANTAN DE <sayantande@MacBookAir.lan>
2025-03-16 12:35:27 +0530

    dfv

--- games/urls.py
+++ games/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('game_room/<int:game_id>/', views.game_room_views, name='game_room_views'),
+    #path('game/<str:game_id>/', views.game_room_views, name='game_room'),
 
     path('game_lobby/<str:room_code>/<str:game_id>/', views.game_lobby, name='game_lobby'),  # Include game_code in the URL
     path('start_game/<str:room_code>/<str:game_id>/', views.start_game, name='start_game'),  # Include game_code in the URL
