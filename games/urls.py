fc81d7f2bf0a08e939364d7cf294ea985821e69f
Shruti Prasad <shruti141203@gmail.com>
2025-04-13 18:11:54 +0530

    update pull

--- games/urls.py
+++ games/urls.py
@@ -14,16 +14,12 @@ urlpatterns = [
     path('chess/<str:room_code>', views.chess, name='chess'),
     # path('agar/', views.agar, name='agar'),
     path('snake/<str:room_code>', views.snake, name='snake'),
-<<<<<<< HEAD
-    path('get_players/<str:room_code>/', views.get_players, name='get_players')
-=======
     path('ludo/<str:room_code>',views.ludo, name='ludo'),
     path('get_players/<str:room_code>/', views.get_players, name='get_players'),
      path('check_game_starting/<str:room_code>/', views.check_game_starting, name='check_game_starting'),
     path('start_game_countdown/<str:room_code>/<str:game_id>/', views.start_game_countdown, name='start_game_countdown'),
     # ... your other URL patterns ...
 
->>>>>>> 3139c241f10c3e9062b3f79bb2e1d916f9630e1f
 ]
 
 
